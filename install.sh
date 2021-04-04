f314b338091ca211cabde21fba427192c7a860d5
ryo.shiraishi <ryo.shiraishi6352@gmail.com>
2021-04-04 13:24:06 +0900

    add nvim

--- install.sh
+++ install.sh
@@ -5,10 +5,15 @@ SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 
 #bash
 BASH_DIR=$SCRIPT_DIR/bash
-ln -s $BASH_DIR/bashrc ~/.bashrc
+ln -fs $BASH_DIR/bashrc ~/.bashrc
 
 #fish
 FISH_DIR=$SCRIPT_DIR/fish
 mkdir -p ~/.config/fish/functions
-ln -s $FISH_DIR/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+ln -fs $FISH_DIR/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+
+#nvim
+NVIM_DIR=$SCRIPT_DIR/nvim
+mkdir -p ~/.config/nvim
+ln -fs $NVIM_DIR/init.vim ~/.config/nvim/init.vim
 
---
+++ nvim/init.vim
@@ -0,0 +1,4 @@
+set hidden
+set hlsearch
+set number
+
