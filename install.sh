c697e6834c2fad76978144db971aed5b59e02fe1
ryo.shiraishi <ryo.shiraishi6352@gmail.com>
2021-04-09 21:36:29 +0900

    add bash_profile

--- install.sh
+++ install.sh
@@ -5,20 +5,21 @@ SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 
 #bash
 BASH_DIR=$SCRIPT_DIR/bash
-ln -fs $BASH_DIR/bashrc ~/.bashrc
+ln -is $BASH_DIR/bashrc ~/.bashrc
+ln -is $BASH_DIR/bash_profile ~/.bash_profile
 
 #fish
 FISH_DIR=$SCRIPT_DIR/fish
 mkdir -p ~/.config/fish/functions
-ln -fs $FISH_DIR/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+ln -is $FISH_DIR/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 
 #nvim
 NVIM_DIR=$SCRIPT_DIR/nvim
 mkdir -p ~/.config/nvim
-ln -fs $NVIM_DIR/init.vim ~/.config/nvim/init.vim
+ln -is $NVIM_DIR/init.vim ~/.config/nvim/init.vim
 
 #tmux
 TMUX_DIR=$SCRIPT_DIR/tmux
 mkdir -p ~/.config/tmux
-ln -fs $TMUX_DIR/tmux.conf ~/.config/tmux/tmux.conf
+ln -is $TMUX_DIR/tmux.conf ~/.config/tmux/tmux.conf
 
